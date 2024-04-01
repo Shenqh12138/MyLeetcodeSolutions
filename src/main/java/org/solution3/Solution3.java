34898cbcf5f9fe0510760b365f3a0eff02045109
shenqh12138 <1416920531@qq.com>
2024-04-01 11:42:55 +0800

    滑动窗口

--- src/main/java/org/solution3/Solution3.java
+++ src/main/java/org/solution3/Solution3.java
@@ -45,8 +45,24 @@ public class Solution3 {
         return max;
     }
 
+    public int lengthOfLongestSubstring3(String s) {
+        int max = 0;
+        int left = 0;
+        HashMap<Character, Integer> hashMap = new HashMap<>();
+        for (int i = 0;i<s.length();i++){
+            if (hashMap.containsKey(s.charAt(i))){
+                // 这里取max实际上是为了防止乱跳，如果窗口内的元素重复才会更改left的值，否则不更改。
+                left = Math.max(left, hashMap.get(s.charAt(i)) + 1);
+            }
+            hashMap.put(s.charAt(i), i);
+            max = Math.max(max, i - left + 1);
+        }
+
+        return max;
+    }
+
     @Test
     public void Test() {
-        System.out.println(lengthOfLongestSubstring2("dasdsdadasqqwerczsd"));
+        System.out.println(lengthOfLongestSubstring3("dasdsdadasqqwerczsd"));
     }
 }
